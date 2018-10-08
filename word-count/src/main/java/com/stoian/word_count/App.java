3594032ae8780ae3a59d682a948f905d0650bf61
Unknown <stoian_ivanov_ivanov@abv.bg>
2018-10-08 23:01:56 +0300

    add word count example

---
+++ word-count/src/main/java/com/stoian/word_count/App.java
@@ -0,0 +1,61 @@
+package com.stoian.word_count;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+        
+        KStream<String, String> textLines = builder.stream("word-count");
+        KTable<String, Long> wordCounts = textLines
+                .mapValues(textLine -> textLine.toLowerCase())
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+                .selectKey((key, word) -> word)
+                .groupByKey()
+                .count("Counts");
+
+        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
+
+
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        
+        // Update:
+        // print the topology every 10 seconds for learning purposes
+        while(true){
+            System.out.println(streams.toString());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+    }
+}
